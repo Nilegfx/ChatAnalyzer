c61329ca0a51577218d340f119d4cfc399ca9c7d
ahmed ayoub <ahmed.mahmoud@zalando.de>
2018-05-22 08:43:43 +0200

    use npm for dependencies, fix global variable errors, move things around and delete unnecessary js files

---
+++ client/index.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'jquery.easing/jquery.easing.min';
+import 'chart.js/dist/Chart.bundle.min';
+import './modules/feedback';
+import './modules/form';
+import './modules/analyze';
+import './locals/stopwords';
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,86 @@
-const path = require( 'path' );
+const path = require('path');
+const webpack = require('webpack');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+const CopyWebpackPlugin    = require( 'copy-webpack-plugin' );
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  entry  : path.join( __dirname, 'app/index.js' ),
-  output : {
-    path      : path.resolve( 'build' ),
-    filename: 'main.bundle.js',
-    publicPath: '/'
+  devtool: 'eval',
+  entry: [
+    'font-awesome/scss/font-awesome.scss',
+    './client/css/main.scss',
+    './client/index.js'
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: '[name].js'
   },
-  module : {
+  devServer: {
+    contentBase: 'build/', // Relative directory for base of server
+    publicPath: '/',
+    inline: true,
+    port: process.env.PORT || 3000, // Port Number
+    host: '127.0.0.1', // Change to '0.0.0.0' for external facing server
+    historyApiFallback: true,
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery',
+      'moment': 'moment'
+    }),
+    new MiniCssExtractPlugin('main.css'),
+    new CopyWebpackPlugin( [ { from: 'client/index.html' },{ from: 'client/images', to: 'images' } ], { ignore: [ '.DS_Store' ] } ),
+    new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
+    // new BundleAnalyzerPlugin()
+  ],
+  module: {
     rules: [
       {
-        test   : /\.jsx?$/,
-        include: [
-          path.resolve( 'src' )
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          cacheDirectory: true,
+        },
+      },
+      {
+        test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
         ],
-        use    : [
-          {
-            loader: 'babel-loader'
-          }
+      },
+      {
+        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        use: 'url-loader?limit=10000',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'file-loader?name=images/[name].[ext]',
+          'image-webpack-loader?bypassOnDebug'
+        ]
+      },
+      // font-awesome
+      {
+        test: /font-awesome\.config\.js/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'font-awesome-loader' }
         ]
       }
     ]
-  },
-  resolve: {
-    extensions: [ '.js', '.jsx', '.json' ]
   }
 };
